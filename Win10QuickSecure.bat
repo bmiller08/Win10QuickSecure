7b44857104958a2efdd933ca59ac808cad248032
bmiller08 <bmiller@renovo1.com>
2015-08-10 11:49:25 -0400

    AddMoreSettings
    Add more security settings.

--- Win10QuickSecure.bat
+++ Win10QuickSecure.bat
@@ -1,2 +1,7 @@
 @echo off
 for /r . %%i in (*.reg) do regedit.exe /s "%%i"
+
+sc delete DiagTrack
+sc delete dmwappushservice
+echo "" > C:\ProgramData\Microsoft\Diagnosis\ETLLogs\AutoLogger\AutoLogger-Diagtrack-Listener.etl
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v AllowTelemetry /t REG_DWORD /d 0 /f
